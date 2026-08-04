a7fb5a6ca7f61b60ff8ebe5c511b885ce45a9d12
Muhammad Bappi <abappi2019@gmail.com>
2026-08-04 15:04:53 +0600

    fix(@bruin/devtools): raise bottom sheet max height to 90%

--- packages/@bruin/devtools/src/components/ui/bottom-sheet.ui.tsx
+++ packages/@bruin/devtools/src/components/ui/bottom-sheet.ui.tsx
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    maxHeight: '80%',
+    maxHeight: '90%',
     backgroundColor: COLORS.background,
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
